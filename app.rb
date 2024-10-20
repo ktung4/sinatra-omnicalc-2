c628cfa8b160e480dad1b9254b95b4774777cff2
ktung4 <ktung@chicagobooth.edu>
2024-10-20 22:11:54 +0000

    sinatra: omnicalc 2

--- app.rb
+++ app.rb
@@ -28,17 +28,23 @@ get("/wizard_sub") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num / @second_num
   erb(:div_result)
 end
